065c1e92441eee8e0454ca7516b768504daa325d
David Ke Hong <hongkedavid@gmail.com>
2015-11-13 05:15:47 -0500

    Create hp_van_template.sh

---
+++ hp_van_template.sh
@@ -0,0 +1,4 @@
+bin/gen-sdn-app --directory ~/workspace/hybrid/ --template skeleton \
+--app hybrid --company hp --subject HybridSDN --app-name "Hybrid SDN" \
+--company-name "Hewlett Packard" --description "Application for managing\ 
+a hybrid SDN network" --rest-path hybrid
