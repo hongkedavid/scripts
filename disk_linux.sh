d144ba613dcc75699bc4363137323f4b08bd071c
David Ke Hong <hongkedavid@gmail.com>
2018-03-02 17:51:38 -0500

    Update disk_linux.sh

--- disk_linux.sh
+++ disk_linux.sh
@@ -1,5 +1,9 @@
 # Find symbolic links recrusively from current directory
+# Ref: https://stackoverflow.com/questions/8513133/how-do-i-find-all-of-the-symlinks-in-a-directory-tree
 ls -lR . | grep ^l
 
 # List disk usage of current directory
 sudo du --max-depth=1 -lh
+
+# List usage of different partitions
+df -lh 
