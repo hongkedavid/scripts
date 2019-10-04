b5a741db378cfc9b05c9d6798d356eac8fb3e7bf
David Ke Hong <hongkedavid@gmail.com>
2019-10-04 11:14:10 -0400

    Update adduser.sh

--- adduser.sh
+++ adduser.sh
@@ -3,7 +3,7 @@ sudo useradd -d /home/$USERNAME -m $USERNAME -s /bin/bash
 sudo mkdir /home/$USERNAME/.ssh
 sudo vi .ssh/authorized_keys
 sudo chmod 644 .ssh/authorized_keys
-sudo chown -R $USERNAME:$USERNAME /home/david/.ssh
+sudo chown -R $USERNAME:$USERNAME /home/$USERNAME/.ssh
 
 # Grant sudo access
 sudo vi /etc/sudoers
