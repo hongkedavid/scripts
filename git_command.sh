cef56b67626ac68dab0e4d64824b131081687c93
David Ke Hong <hongkedavid@gmail.com>
2019-11-13 14:00:24 -0500

    Update git_command.sh

--- git_command.sh
+++ git_command.sh
@@ -1,3 +1,11 @@
 # Count LOC of a repo
 # Ref: https://stackoverflow.com/questions/4822471/count-number-of-lines-in-a-git-repository
 git ls-files | xargs wc -l
+
+# Diff file across branches
+# Ref: https://stackoverflow.com/questions/4099742/how-to-compare-files-from-two-different-branches
+git diff $branch1 $branch2 -- $file
+
+# Diff file across tags
+# Ref: https://stackoverflow.com/questions/3211809/how-to-compare-two-tags-with-git
+git diff $tag1 $tag2 -- $file
