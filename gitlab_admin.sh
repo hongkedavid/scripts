389223833e773285b3be7b56bc8790ea314bdae0
David Ke Hong <hongkedavid@gmail.com>
2020-02-24 15:22:19 -0500

    Create gitlab_admin.sh

---
+++ gitlab_admin.sh
@@ -0,0 +1,3 @@
+# Renew letsencrypt certificate https://docs.gitlab.com/omnibus/settings/ssl.html#automatic-lets-encrypt-renewal
+# Acme issue fix https://gitlab.com/gitlab-org/gitlab/issues/38255
+sudo gitlab-ctl reconfigure
