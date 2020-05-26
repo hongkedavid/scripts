76f891bf5a4a53e8c4bba60777d5caec389359ac
David Ke Hong <hongkedavid@gmail.com>
2020-05-26 17:18:55 -0400

    Update adduser.sh

--- adduser.sh
+++ adduser.sh
@@ -1,7 +1,7 @@
 # Add a user named $USERNAME in Ubuntu
 sudo useradd -d /home/$USERNAME -m $USERNAME -s /bin/bash
 sudo mkdir /home/$USERNAME/.ssh
-sudo vi .ssh/authorized_keys
+sudo vi /home/$USERNAME/.ssh/authorized_keys
 sudo chown -R $USERNAME:$USERNAME /home/$USERNAME/.ssh
 
 # Grant sudo access
